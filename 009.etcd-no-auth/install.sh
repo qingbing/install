b0a87d916907006717067ae2e510cbd2578f8c44
duqingbing <duqingbing@ghostcloud.cn>
2023-03-16 10:39:26 +0800

    etcd-no-auth调整

--- 009.etcd-no-auth/install.sh
+++ 009.etcd-no-auth/install.sh
@@ -9,6 +9,7 @@ PLUGIN_NAME=etcd
 fileConf=(default:etcd-v3.5.6-linux-amd64.tar.gz type:string desc:etcd二进制包 required:true)
 clusterConf=(default:etcd=http://${LOCAL_IP}:2380 type:string desc:etcd集群初始化连接,只能使用2380端口 eg:node1=http://ip1:2380,node2=http://ip2:2380)
 clusterTokenConf=(default:etcd-cluster type:string desc:集群token)
+clusterStateConf=(default:new type:string desc:当前集群状态)
 nameConf=(default:etcd type:string desc:etcd节点名,必须和集群初始化连接中的节点名对应一致 eg:etcd-node1)
 ipConf=(default:"${LOCAL_IP}" type:string desc:本节点IP eg:"${LOCAL_IP}")
 installDirConf=(default:/opt/etcd type:string desc:安装目录)
@@ -16,6 +17,7 @@ PARAM_CONFS=(
     ["f"]=fileConf
     ["cluster"]=clusterConf
     ["clusterToken"]=clusterTokenConf
+    ["clusterState"]=clusterStateConf
     ["name"]=nameConf
     ["ip"]=ipConf
     ["installDir"]=installDirConf
@@ -26,6 +28,7 @@ ParseParams
 tarFile=${PC["f"]}
 cluster=${PC["cluster"]}
 clusterToken=${PC["clusterToken"]}
+clusterState=${PC["clusterState"]}
 name=${PC["name"]}
 ip=${PC["ip"]}
 installDir=${PC["installDir"]}
@@ -55,6 +58,7 @@ function conf() {
         -e "s#{{ADVERTISE_CLIENT_URLS}}#http://${ip}:2379#g" \
         -e "s#{{INITIAL_CLUSTER}}#${cluster}#g" \
         -e "s#{{INITIAL_CLUSTER_TOKEN}}#${clusterToken}#g" \
+        -e "s#{{INITIAL_CLUSTER_STATE}}#${clusterState}#g" \
         templates/default.conf >"${installDir}"/conf/default.conf
 
     sed \
@@ -62,7 +66,7 @@ function conf() {
         -e "s#{{CONF_DIR}}#${installDir}/conf#g" \
         templates/${PLUGIN_NAME}.service >"${SYS_CONF_DIR}"/"${PLUGIN_NAME}".service
 
-    # systemctl unmask ${PLUGIN_NAME}
+    systemctl unmask ${PLUGIN_NAME}
     systemctl daemon-reload
     systemctl enable ${PLUGIN_NAME}
 }
